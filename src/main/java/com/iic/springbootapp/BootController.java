704c5ea711564a00fa0ae63b0d72a736212394ee
sivakrishna <chaitanyasiva84@gmail.com>
2016-06-22 19:10:19 +0530

    one change

--- src/main/java/com/iic/springbootapp/BootController.java
+++ src/main/java/com/iic/springbootapp/BootController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BootController
 {
-   @RequestMapping("/req")
+   @RequestMapping("/reqt")
     public String sample()
    {
        return "Spring Boot App";
